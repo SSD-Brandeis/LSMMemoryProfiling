6b64a8f58bf95467fcbf9eff1a7d0726d7e7883e
James Chen <boaochen@brandeis.edu>
2026-08-04 20:48:23 +0000

    minor

---
+++ plot_scripts/memtable_style.py
@@ -0,0 +1,27 @@
+
+MEMTABLE_DISPLAY_NAMES = {
+    "vector": "V-Qsort",
+    "unsorted_vector": "V-Qscan",
+    "sorted_vector": "V-Sorted",
+    "linklist": "Link-L",
+    "simple_skiplist": "Skip-L",
+    "skiplist": "InSkip-L",
+    "hash_skiplist": "Hash-SL",
+    "hash_linklist": "Hash-LL",
+    "hash_vector": "Hash-V",
+    "tlx_btree": "b+tree",
+}
+
+MEMTABLE_COLORS = {
+    "skiplist": "#2a78d6",
+    "simple_skiplist": "#008300",
+    "vector": "#e87ba4",
+    "unsorted_vector": "#eda100",
+    "sorted_vector": "#8a5a2b",
+    "linklist": "#c9a227",
+    "hash_skiplist": "#7a3b8f",
+    "hash_linklist": "#2fa39a",
+    "hash_vector": "#b5533c",
+    "art": "#1baf7a",
+    "tlx_btree": "#4a3aa7",
+}
