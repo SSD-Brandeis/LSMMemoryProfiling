7009024799dd7d0cc7496f786945c0dd6b1710d3
James <boaochen@brandeis.edu>
2026-08-11 16:43:56 +0000

    minor

--- src/run_workload.cc
+++ src/run_workload.cc
@@ -36,14 +36,14 @@ int runWorkload(std::unique_ptr<DBEnv> &env) {
   std::shared_ptr<Buffer> buffer = std::make_unique<Buffer>(buffer_file);
   std::unique_ptr<Buffer> stats = std::make_unique<Buffer>(stats_file);
 
-  // // Add custom listners
-  // std::shared_ptr<CompactionsListner> compaction_listener =
-  //     std::make_shared<CompactionsListner>(env);
-  // options.listeners.emplace_back(compaction_listener);
+  // Add custom listners
+  std::shared_ptr<CompactionsListner> compaction_listener =
+      std::make_shared<CompactionsListner>(env);
+  options.listeners.emplace_back(compaction_listener);
 
-  // std::shared_ptr<FlushListner> flush_listener =
-  //     std::make_shared<FlushListner>(buffer);
-  // options.listeners.emplace_back(flush_listener);
+  std::shared_ptr<FlushListner> flush_listener =
+      std::make_shared<FlushListner>(buffer);
+  options.listeners.emplace_back(flush_listener);
 
   if (env->IsDestroyDatabaseEnabled()) {
     DestroyDB(env->kDBPath, options);
@@ -284,8 +284,8 @@ int runWorkload(std::unique_ptr<DBEnv> &env) {
           if (it->key().ToString() >= end_key) {
             break;
           }
-        // std::cout << "RQ: Key: " << it->key().ToString()
-        //           << " Value: " << it->value().ToString() << std::endl;
+          // std::cout << "RQ: Key: " << it->key().ToString()
+          //           << " Value: " << it->value().ToString() << std::endl;
         }
       }
 
